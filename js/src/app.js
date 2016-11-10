4e7daad0ad4bec93bc524ae5d87897be13a4b4f2
ryanbmarx <ryanbmarx@gmail.com>
2016-11-10 17:50:39 -0600

    Clean up debug messages

--- js/src/app.js
+++ js/src/app.js
@@ -2,22 +2,30 @@ import * as d3 from "d3";
 
 
 function getFill(winner){
-	const opacity = .5
+	const opacity = .25
 	return winner == "trump" ? `rgba(255, 0, 0, ${opacity})` : `rgba(0, 0, 255, ${opacity})`;
 }
 
-function drawMap(dataSelection, data, map){
-	console.log(`drawing map of ${dataSelection}`, data);
+function drawMap(dataSelection, data, map, dataType){
+	console.log(`drawing map of ${dataSelection}`);
 
 	// Define scale ... use SQRT b/c these are circles and it is their AREA which must scale, not the r
-	const r = d3.scaleSqrt() 
-		.domain([0, d3.max(data.features, d => d['properties'][dataSelection])])
-		.range([0, 20]); 
+	const maxCircleSize = dataType == "pct" ? 15 : 40;
+
+	const dmax = dataType == "pct" ? 1 : d3.max(data.features, d => {
+		return parseFloat(d['properties'][dataSelection]);
+	});
+
+	const dmin = d3.min(data.features, d => {
+		return parseFloat(d['properties'][dataSelection]);
+	});
+
+	const r = d3.scaleLinear() 
+		.domain([0, dmax])
+		.range([0, maxCircleSize]); 
 
 	// Draw boundaries
-
 	let demoMap = d3.select(map).select('.map__container');
-;
 
 	const containerBox = demoMap.node().getBoundingClientRect(),
 		height = containerBox.height,
@@ -73,19 +81,30 @@ function drawMap(dataSelection, data, map){
 			.enter()
 				.append( "path" )
 					.classed('midwest-map', true)
-					.style('fill', '#e0e0e0')
+					.style('fill', function(d){
+						let winner = d.properties.clinton_trump;
+						// console.log(winner);
+						return '#e0e0e0';
+						return getFill(winner);
+					})
 					.style('stroke', '#ffffff')
 					.style('stroke-width', 1)
 					.attr( "d", midwestGeoPath)
 					.each((d)=>{
 						let centroid = d.properties.centroid_coordinates;
-						let winner = d.properties.voting_clinton_trump;
+						let winner = d.properties.clinton_trump;
 						let stat = d.properties[dataSelection];
 						demoMap.select('.circles')
 							.append('circle')
 							.attr('cx', projection(centroid)[0])
 							.attr('cy', projection(centroid)[1])
-							.attr('r',r(stat))
+							.attr('r',function(){
+								// console.log(d.properties.GEOID, stat, r(stat))	
+								
+								return r(parseFloat(stat));
+							})
+							.attr('data-county', `${d.properties.census_name}, ${d.properties.state}`)
+							.attr('data-stat',stat)
 							.style('fill', getFill(winner));
 					})
 		demoMap.select('.state-boundaries')
@@ -108,14 +127,14 @@ window.onload = function(){
 		window.STATE_BOUNDARIES = data;
 	})
 
-	d3.json(`http://${window.ROOT_URL}/data/test-with-centroid.geojson`, (data) => {
+	d3.json(`http://${window.ROOT_URL}/data/midwest-counties-with-centroid.geojson`, (data) => {
 		console.log(data);
 		document.querySelectorAll('.map').forEach((map, idx)=>{
 		let dataSelection = map.dataset.map;
-		
+		let dataType = map.dataset.type;
 		// This logic is just for byulding while the data remains out.
 		// if (dataSelection != "x"){
-			drawMap(dataSelection,data, map);
+			drawMap(dataSelection,data, map, dataType);
 		// }
 
 	})	
